94e6327da1c1dc839f74f7d2b7a40f25613398fe
Raconer <cha11enge@hanmail.net>
2023-05-30 23:19:51 +0900

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+import app from './app';
+import debug from 'debug';
+import http from 'http';
+
+const PORT = normalizePort(process.env.PORT || '3000');
+app.set('port', PORT);
+
+const server = http.createServer(app);
+
+server.listen(PORT);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val: string): string | number | boolean {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error: any) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof PORT === 'string' ? 'Pipe ' + PORT : 'Port ' + PORT;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr?.port;
+  debug('Listening on ' + bind);
+}
